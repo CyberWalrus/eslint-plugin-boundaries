fd78e66652a7b5988b4ff43a83adb4ec22c29f92
Пахомов Андрей Николаевич <andrey.pahomov@ligastavok.ru>
2025-02-11 02:35:47 +0300

    add rule slice

--- src/constants/rules.js
+++ src/constants/rules.js
@@ -2,6 +2,7 @@ module.exports = {
   ELEMENT_TYPES: "element-types",
   ENTRY_POINT: "entry-point",
   EXTERNAL: "external",
+  SLICE: "slice",
   NO_IGNORED: "no-ignored",
   NO_PRIVATE: "no-private",
   NO_UNKNOWN_FILES: "no-unknown-files",
